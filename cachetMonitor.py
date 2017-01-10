cf5fb585939e057997b733082d67c9a6213da559
Gareth <gaz492@gmail.com>
2017-01-10 19:49:08 +0000

    Switched to using config file

--- cachetMonitor.py
+++ cachetMonitor.py
@@ -4,31 +4,26 @@ import urllib3
 import certifi
 import httplib
 import json
-from pprint import pprint
-
-# Static vars
-
-status_codes = [200, 201, 202, 203, 204, 205, 206, 208, 226]
-x = 0
 
 # Open Json File
 with open('config.json', 'r') as json_data:
     data = json.load(json_data)
 
+# Static vars
+x = 0
+
 # Count how many sites to monitor
 monitor_count = len(data['monitoring'])
-
-pprint(data)
 
 # Loop through sites to monitor
 while (x < monitor_count):
 
-    pprint(data['monitoring'][x]['url'])
+    # status_codes = [200, 201, 202, 203, 204, 205, 206, 208, 226]
+    status_codes = data['monitoring'][x]['expected_status_code']
 
     url = data['monitoring'][x]['url']
 
     http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
-
     try:
         r = http.request('GET', url, timeout=data['monitoring'][x]['timeout'])
     except urllib3.exceptions.SSLError as e:
