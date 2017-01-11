fb4c19632e20fbc82b3d175ca25bde756e6b4ff9
Gareth <gaz492@gmail.com>
2017-01-11 14:08:26 +0000

    I dont even

--- cachetMonitor.py
+++ cachetMonitor.py
@@ -18,16 +18,6 @@ class Cachet(object):
         self.http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
         self.checkSites()
 
-        self.cfErrors = {
-            520: "Web server is returning an unknown error",
-            521: "Web server is down",
-            522: "Connection timed out",
-            523: "Origin is unreachable",
-            524: "A timeout occurred",
-            525: "SSL handshake failed",
-            526: "Invalid SSL certificate"
-        }
-
     def checkForIncident(self, component_id):
         current_incidents = self.utils.getIncidents()
         incidents = len(current_incidents.json()['data'])
@@ -48,38 +38,55 @@ class Cachet(object):
         monitor_count = len(self.utils.readConfig()['monitoring'])
         x = 0
 
+        cfErrors = {
+            520: "Web server is returning an unknown error",
+            521: "Web server is down",
+            522: "Connection timed out",
+            523: "Origin is unreachable",
+            524: "A timeout occurred",
+            525: "SSL handshake failed",
+            526: "Invalid SSL certificate"
+        }
+
         # Loop through sites to monitor
         while x < monitor_count:
             status_codes = self.utils.readConfig()['monitoring'][x]['expected_status_code']
             url = self.utils.readConfig()['monitoring'][x]['url']
             c_id = self.utils.readConfig()['monitoring'][x]['component_id']
             try:
-                r = self.http.request('GET', url, timeout=self.utils.readConfig()['monitoring'][x]['timeout'])
+                r = self.http.request('GET', url, retries=False, timeout=self.utils.readConfig()['monitoring'][x]['timeout'])
             except urllib3.exceptions.SSLError as e:
+                print e
+            except urllib3.exceptions.MaxRetryError as e:
+                print e
+            except urllib3.exceptions.NewConnectionError as e:
                 print e
             except urllib3.exceptions.HTTPError as e:
                 print e
             else:
-                if r.status not in status_codes and r.status not in self.cfErrors:
-                    print url
+                if r.status not in status_codes and r.status not in cfErrors:
                     error_code = '%s HTTP Error %s: %s' % (url, r.status, httplib.responses[r.status])
-                    print error_code
                     self.utils.putComponentsByID(c_id, status=4)
-                elif r.status in self.cfErrors and r.status not in status_codes:
-                    error_code = '%s HTTP Error %s: %s' % (url, r.status, self.cfErrors[r.status])
-                    print error_code
+                    self.__reportIncident('%s: HTTP Error' % url, error_code, 2, 1, 5, 1)
+                elif r.status in cfErrors and r.status not in status_codes:
+                    error_code = '%s HTTP Error %s: %s' % (url, r.status, cfErrors[r.status])
+                    self.__reportIncident('%s: HTTP Error' % url, error_code, 2, 1, 5, 1)
                     self.utils.putComponentsByID(c_id, status=4)
                 elif r.status in status_codes:
-                    try:
-                        current_status = self.utils.getComponentsByID(c_id).json()['data']['status']
-                        if current_status not in status_codes:
-                            print('component id: %s' % c_id)
-                            self.utils.putComponentsByID(c_id, status=1)
-                            incident_id, incident_status = self.checkForIncident(c_id)
-
-                            if incident_id and incident_status:
-                                print "active incident"
-                    except requests.exceptions.SSLError as e:
-                        print(e)
+                    current_status = self.utils.getComponentsByID(c_id).json()['data']['status']
+                    if current_status not in status_codes:
+                        print('component id: %s' % c_id)
+                        self.utils.putComponentsByID(c_id, status=1)
+                        # incident_id, incident_status = self.checkForIncident(c_id)
+                        #
+                        # if incident_id and incident_status:
+                        #     print "active incident"
             x += 1
+
+    def __reportIncident(self, title, description, status, visible, c_id, c_status):
+        print("Title: %s, Description: %s, Status: %s, Visible: %s, Component ID: %s, Component Status: %s" % (title, description, str(status), str(visible), str(c_id), str(c_status)))
+
+    def __updateIncident(self, i_id, description, status, c_status):
+        print("Incident ID: %s, Description: %s, Status: %s, Component Status: %s" % (i_id, description, status, c_status))
+
 Cachet()
