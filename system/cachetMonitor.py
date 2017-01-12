c2c2b8020c5b2491fc89840628ad1f523261f743
Gareth <gaz492@gmail.com>
2017-01-12 18:06:29 +0000

    Added error reporting to all exeptions

--- system/cachetMonitor.py
+++ system/cachetMonitor.py
@@ -55,68 +55,68 @@ class Cachet(object):
             c_id = self.utils.readConfig()['monitoring'][x]['component_id']
             localtime = time.asctime(time.localtime(time.time()))
             try:
-                r = self.http.request(request_method, url, retries=1,
-                                      timeout=self.utils.readConfig()['monitoring'][x]['timeout'])
+                r = self.http.request(request_method, url, retries=1, timeout=self.utils.readConfig()['monitoring'][x]['timeout'])
             except urllib3.exceptions.SSLError as e:
-                print e
                 incident_id = self.checkForIncident(c_id)
-                if incident_id:
-                    print('Incident found')
+                if not incident_id:
+                    c_status = 4
+                    error_code = '%s check **failed** - %s \n\n`%s %s SSL Error: %s`' % (url, localtime, request_method, url, e)
+                    incident_id = self.checkForIncident(c_id)
+                    if not incident_id:
+                        self.__reportIncident('%s: SSL Error' % url, error_code, 1, c_id, c_status)
             except urllib3.exceptions.MaxRetryError as e:
-                print e
+                c_status = 4
+                error_code = '%s check **failed** - %s \n\n`%s %s Max Retry Error: %s`' % (url, localtime, request_method, url, e)
+                incident_id = self.checkForIncident(c_id)
+                if not incident_id:
+                    self.__reportIncident('%s: Max Retry Error' % url, error_code, 1, c_id, c_status)
             except urllib3.exceptions.NewConnectionError as e:
-                print e
+                c_status = 4
+                error_code = '%s check **failed** - %s \n\n`%s %s New Connection Error: %s`' % (
+                url, localtime, request_method, url, e)
+                incident_id = self.checkForIncident(c_id)
+                if not incident_id:
+                    self.__reportIncident('%s: New Connection Error' % url, error_code, 1, c_id, c_status)
             except urllib3.exceptions.HTTPError as e:
-                print e
+                c_status = 4
+                error_code = '%s check **failed** - %s \n\n`%s %s HTTP Error: %s`' % (
+                    url, localtime, request_method, url, e)
+                incident_id = self.checkForIncident(c_id)
+                if not incident_id:
+                    self.__reportIncident('%s: HTTP Error' % url, error_code, 1, c_id, c_status)
             else:
                 if r.status not in status_codes and r.status not in cfErrors:
-                    error_code = '%s check **failed** - %s \n\n`%s %s HTTP Error %s: %s`' % (
-                    url, localtime, request_method, url, r.status, httplib.responses[r.status])
+                    error_code = '%s check **failed** - %s \n\n`%s %s HTTP Error %s: %s`' % (url, localtime, request_method, url, r.status, httplib.responses[r.status])
                     c_status = 4
                     incident_id = self.checkForIncident(c_id)
                     if not incident_id:
-                        print('Incident not found')
                         self.__reportIncident('%s: HTTP Error' % url, error_code, 1, c_id, c_status)
-                    else:
-                        self.getIncidentInfo(incident_id)
                     self.utils.putComponentsByID(c_id, status=c_status)
                 elif r.status in cfErrors and r.status not in status_codes:
-                    error_code = '%s check **failed** - %s \n\n`%s %s HTTP Error %s: %s`' % (
-                    url, localtime, request_method, url, r.status, cfErrors[r.status])
+                    error_code = '%s check **failed** - %s \n\n`%s %s HTTP Error %s: %s`' % (url, localtime, request_method, url, r.status, cfErrors[r.status])
                     c_status = 4
                     incident_id = self.checkForIncident(c_id)
                     if not incident_id:
-                        print('Incident not found')
                         self.__reportIncident('%s: HTTP Error' % url, error_code, 1, c_id, c_status)
-                    else:
-                        self.getIncidentInfo(incident_id)
                     self.utils.putComponentsByID(c_id, status=c_status)
                 elif r.status in status_codes:
                     current_status = self.utils.getComponentsByID(c_id).json()['data']['status']
                     if current_status not in status_codes:
-                        print('component id: %s' % c_id)
                         self.utils.putComponentsByID(c_id, status=1)
                         incident_id = self.checkForIncident(c_id)
                         if incident_id:
-                            print('Incident found changing status to resolved')
-                            incident_description = "Resolved at %s\n\n***\n\n%s" % (
-                            localtime, self.getIncidentInfo(incident_id))
+                            incident_description = "Resolved at %s\n\n***\n\n%s" % (localtime, self.getIncidentInfo(incident_id))
                             self.__updateIncident(incident_id, incident_description, 4, c_id, 1)
             x += 1
 
     def __reportIncident(self, title, description, status, c_id, c_status):
-        print("Title: %s, Description: %s, Status: %s, Component ID: %s, Component Status: %s" % (
-        title, description, str(status), str(c_id), str(c_status)))
         self.utils.postIncidents(title, description, status, 1, component_id=c_id, component_status=c_status)
 
     def __updateIncident(self, i_id, description, status, c_id, c_status):
-        print("Incident ID: %s, Description: %s, Status: %s, Component ID: %s, Component Status: %s" % (
-        i_id, description, status, c_id, c_status))
         self.utils.putIncidentsByID(i_id, message=description, status=status, component_id=c_id,
                                     component_status=c_status)
 
     def getIncidentInfo(self, i_id):
-        print (self.utils.getIncidentsByID(i_id).json())
         incident = self.utils.getIncidentsByID(i_id).json()
         i_description = incident['data']['message']
         return i_description
