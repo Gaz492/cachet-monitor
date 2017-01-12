7ba0228d6fad70f50f3d0e66cbb459c3acc13fb3
Gareth <gaz492@gmail.com>
2017-01-12 21:05:49 +0000

    changed code order

--- run.py
+++ run.py
@@ -7,17 +7,17 @@ from apscheduler.triggers.date import DateTrigger
 from system.cachetMonitor import Cachet
 from system.utils import Utils
 
-utils = Utils()
-schedule_interval = utils.readConfig()['interval']
-use_schedule = utils.readConfig()['use_schedule']
-
-logging.basicConfig()
-
 if not os.path.exists("settings/config.json"):
     print("|! Couldn't find config.json!")
     print("|! rename the config.json.example to config.json and edit it as required.")
     print("|! After that, run the script again.")
     exit(1)
+
+utils = Utils()
+schedule_interval = utils.readConfig()['interval']
+use_schedule = utils.readConfig()['use_schedule']
+
+logging.basicConfig()
 
 scheduler = BlockingScheduler()
 scheduler.add_job(Cachet, trigger=DateTrigger(run_date=datetime.datetime.now()), id='initial')
