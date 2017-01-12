795f4f26160ff55cded0ea1acae91dbff3d15503
Gareth <gaz492@gmail.com>
2017-01-12 14:10:27 +0000

    added option to use schedule

--- run.py
+++ run.py
@@ -9,6 +9,7 @@ from system.utils import Utils
 
 utils = Utils()
 schedule_interval = utils.readConfig()['interval']
+use_schedule = utils.readConfig()['use_schedule']
 
 logging.basicConfig()
 
@@ -22,8 +23,10 @@ scheduler = BlockingScheduler()
 scheduler.add_job(Cachet, trigger=DateTrigger(run_date=datetime.datetime.now()), id='initial')
 scheduler.add_job(Cachet, 'interval', seconds=schedule_interval, id='constant')
 print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
-
-try:
-    scheduler.start()
-except (KeyboardInterrupt, SystemExit):
-        pass
+if use_schedule:
+    try:
+        scheduler.start()
+    except (KeyboardInterrupt, SystemExit):
+            pass
+else:
+    Cachet()
