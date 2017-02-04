74e3711583239fc50f5f95c984ab2586bad6a32f
Gareth <gaz492@gmail.com>
2017-02-04 10:50:22 +0000

    Added Licence

--- run.py
+++ run.py
@@ -7,6 +7,22 @@ from apscheduler.triggers.date import DateTrigger
 from system.cachetMonitor import Cachet
 from system.utils import Utils
 
+'''
+   Copyright 2017 Gareth Williams
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+'''
+
 if not os.path.exists("settings/config.json"):
     print("|! Couldn't find config.json!")
     print("|! rename the config.json.example to config.json and edit it as required.")
--- system/cachetMonitor.py
+++ system/cachetMonitor.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # coding=utf-8
+
 import urllib3
 import certifi
 import httplib
@@ -7,6 +8,22 @@ import time
 
 
 from utils import Utils
+
+'''
+   Copyright 2017 Gareth Williams
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+'''
 
 
 class Cachet(object):
