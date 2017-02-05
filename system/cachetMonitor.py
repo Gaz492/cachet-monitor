0be74e621af0ed75e43689f1b1ba234120323d25
Gareth <gaz492@gmail.com>
2017-02-05 12:57:35 +0000

    http codes
    added list of http codes not supported by httplib.response

--- system/cachetMonitor.py
+++ system/cachetMonitor.py
@@ -29,14 +29,65 @@ from utils import Utils
 
 class Cachet(object):
 
+    httpErrors = {
+        # Cloudflare Errors
+        520: "Web server is returning an unknown error (Cloudflare)",
+        521: "Web server is down (Cloudflare)",
+        522: "Connection timed out (Cloudflare)",
+        523: "Origin is unreachable (Cloudflare)",
+        524: "A timeout occurred (Cloudflare)",
+        525: "SSL handshake failed (Cloudflare)",
+        526: "Invalid SSL certificate (Cloudflare)",
+        # Nginx Errors
+        444: "No Response (Nginx)",
+        494: "Request Header Too Large (Nginx)",
+        495: "Cert Error (Nginx)",
+        496: "No Cert (Nginx)",
+        497: "HTTP to HTTPS (Nginx)",
+        499: "Client Closed Request (Nginx)",
+        # Other
+        # 1xx
+        102: "Server has received and is processing the request",
+        103: "resume aborted PUT or POST requests",
+        122: "URI is longer than a maximum of 2083 characters",
+        # 2xx
+        207: "XML return with possible multiple seperate responses.",
+        208: "The results are previously returned.",
+        226: "The request has been fulfilled amd response is in instance manipulations.",
+        # 3xx
+        308: "Please connect again to the different URL using the same method.",
+        # 4xx
+        418: "I'm a teapot.",
+        420: "Method Failure",
+        421: "Enhance Your Calm",
+        422: "Unprocessable Entity",
+        423: "Locked",
+        424: "Failed Dependency",
+        426: "Upgrade Required",
+        428: "Precondition Required",
+        429: "Too Many Requests",
+        431: "Request Header Fields Too Large",
+        440: "Login Timeout (Microsoft)",
+        449: "Retry With (Microsoft)",
+        450: "Blocked by Windows Parental Controls",
+        451: "Unavailable For Legal Reasons",
+        # 5xx
+        506: "Variant Also Negotiates (RFC 2295)",
+        507: "Insufficient Storage (WebDAV; RFC 4918)",
+        508: "Loop Detected (WebDAV; RFC 5842)",
+        509: "Bandwidth Limit Exceeded (Apache bw/limited extension)",
+        510: "Not Extended (RFC 2774)",
+        511: "Network Authentication Required"
+
+    }
+
     def __init__(self):
+        self.logs = Logger()
         self.utils = Utils()
         self.config = self.utils.readConfig()
         self.base_url = self.config['api_url']
         self.api_token = self.config['api_token']
         self.maxRetries = self.config['retries']
-
-        self.logs = Logger()
 
         self.checkSites()
 
@@ -44,16 +95,6 @@ class Cachet(object):
         # Count how many sites to monitor
         monitor_count = len(self.config['monitoring'])
         x = 0
-
-        cfErrors = {
-            520: "Web server is returning an unknown error",
-            521: "Web server is down",
-            522: "Connection timed out",
-            523: "Origin is unreachable",
-            524: "A timeout occurred",
-            525: "SSL handshake failed",
-            526: "Invalid SSL certificate"
-        }
 
         # Loop through sites to monitor
         while x < monitor_count:
@@ -64,15 +105,16 @@ class Cachet(object):
             request_method = self.config['monitoring'][x]['method']
             c_id = self.config['monitoring'][x]['component_id']
             localtime = time.asctime(time.localtime(time.time()))
+            # current_status = self.getCurrentStatus(c_id)
 
             try:
                 if isEnabled:
                     if request_method.lower() == "get":
                         r = requests.get(url, verify=True)
-                        if r.status_code not in status_codes and r.status_code not in cfErrors:
+                        if r.status_code not in status_codes and r.status_code not in self.httpErrors:
                             self.logs.debug(httplib.responses[r.status_code])
-                        elif r.status_code not in status_codes and r.status_code in cfErrors:
-                            self.logs.debug(cfErrors[r.status_code])
+                        elif r.status_code not in status_codes and r.status_code in self.httpErrors:
+                            self.logs.debug(self.httpErrors[r.status_code])
                         self.logs.debug(url + " " + str(r.status_code))
                     elif request_method.lower() == "post":
                         requests.get(url, verify=True)
